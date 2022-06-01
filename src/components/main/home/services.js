7ebc8de210e0daf611a0da41a77f6c7de5ec4d42
erlanuxi <erlanklink@gmail.com>
2022-06-01 15:45:21 +0600

    era

--- src/components/main/home/services.js
+++ src/components/main/home/services.js
@@ -14,11 +14,11 @@ const Services = () => {
                         <a href="https://t.me/996778067131"><span style={{fontSize: media(15, 20)}}><i className="fa-brands fa-telegram"/> 0778 06 71 31</span></a>
                         <a href="tel:0(552) 615 519"><span style={{fontSize: media(15, 20)}}><i className="fa-solid fa-phone"/> 0778 06 71 31</span></a>
                         <a href="#"><span style={{fontSize: media(15, 20)}}><i className="fa-solid fa-location-dot"/> Кыргыз Республикасы, Ош облусу,
-                            <br/>Чоң-Алай  району, Кызыл-Эшме айылы,<br/>Сулайманов Чолпонбай көчөсү 72</span></a>
+                            <br/>Чоң-Алай  району,<br/>Сулайманов Чолпонбай көчөсү 7</span></a>
                     </div>
                     <div className="services__general--map">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12956.978392942652!2d72.26117880642191!3d39.56217544560513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bec0938d93cee3%3A0xb67c6a1e9b0d69df!2sKyzyleshme!5e0!3m2!1sru!2skg!4v1654026245064!5m2!1sru!2skg"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.339811248766!2d72.19871311522499!3d39.55194481570084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bec7d3f4e8d215%3A0x8daf2978c2ec8442!2z0KfQvtC9LdCQ0LvQsNC5INGA0LDQudC-0L3QtNGD0Log0LHQuNC70LjQvCDQsdC10YDRg9GDINCx0L7Qu9GD0LzRgw!5e0!3m2!1sru!2skg!4v1654075931751!5m2!1sru!2skg"
                             style={{border: "none", height: media(280, 450), width: media(300, 600)}} allowFullScreen=""
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"></iframe>
